462badc2fb898e9b972351c78bc300c6902f8990
hhh101 <149159754+hhh101@users.noreply.github.com>
2023-10-27 13:26:47 +0200

    Create f_e0.py
    not optimized

---
+++ f_e0.py
@@ -0,0 +1,23 @@
+x=[0,1,2,3,4,5,6,7,8,9]
+def expand(n,m):
+  k=n
+  if n[-1]==0:
+    k.pop()
+    return k
+  else:
+    l=n[-1]
+    bp=[]
+    k.pop()
+    p=k[-1]
+    bp.append(p)
+    while p!=l-1:
+      bp.append(p)
+      k.pop()
+      p=k[-1]
+    k.extend(bp*m)
+    return k
+b=1
+while x!=[]:
+  x=expand(x,b)
+  b+=1
+print(b)
