13af142ed517c98185587ceaff44008cb286ecb5
hhh101 <149159754+hhh101@users.noreply.github.com>
2023-10-27 12:39:45 +0200

    Update f_a.py
    i don't know python

--- f_a.py
+++ f_a.py
@@ -1,18 +1,18 @@
 def f(n):
-  return n**n
+  return(n**n)
 def g(n):
   if n==0:
-    return 2:
+    return 2
   else:
     return f(g(n-1))
 def h(n):
   if n==0:
-    return 2:
+    return 2
   else:
     return g(h(n-1))
 def i(n):
   if n==0:
-    return 2:
+    return 2
   else:
     return h(i(n-1))
 print(i(i(i(i(9**9**9)))))
