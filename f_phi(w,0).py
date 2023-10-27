bca699e1b35139f285597f06d5e88ca32ba6b6f6
hhh101 <149159754+hhh101@users.noreply.github.com>
2023-10-27 13:41:37 +0200

    Create f_phi(w,0).py
    idk if it works

---
+++ f_phi(w,0).py
@@ -0,0 +1,34 @@
+x=[0,1,2,3,4,5,6,7,8,9]
+def delta(n,a):
+  f=n
+  i=0
+  for i in range(len(n)):
+    f[i]+=a
+    i+=1
+  return f
+def expand(n,m):
+  k=n
+  if n[-1]==0:
+    k.pop()
+    return k
+  else:
+    l=n[-1]
+    bp=[]
+    k.pop()
+    p=k[-1]
+    bp.append(p)
+    while p>l-1:
+      bp.append(p)
+      k.pop()
+      p=k[-1]
+    delta=p-(l-1)
+    i=1
+    for i in range(a+1):
+      k.extend(delta(bp,delta*i))
+      i+=1
+    return k
+b=1
+while x!=[]:
+  x=expand(x,b)
+  b+=1
+print(b)
