125478da78dceb0cf2aee80b21b62abc24e5e5d6
not hhh i swear <149159754+hhh101@users.noreply.github.com>
2023-11-19 14:32:36 +0100

    Update bigg.py

--- bigg.py
+++ bigg.py
@@ -8,20 +8,19 @@ def f(n,x,m):
     while n[k]>=n[-1] or x[k]>=x[-1]:
       k-=1
     d=n[-1]-n[k]
-    e=x[-1]-n[k]-1
   n.pop()
   x.pop()
   if k>0:
     i=m
     while i>0:
       n.append(n[k]+d)
-      x.append(x[k]+e)
+      x.append(x[k])
       k+=1
       i-=1
   return n, x
 b=3
-x=[-1,0,1]
-y=[-1,0,999]
+x=[-1,0,1,2,3]
+y=[-1,0,1,2,3]
 while x!=[-1]:
   p=f(x,y,b)
   x=p[0]
